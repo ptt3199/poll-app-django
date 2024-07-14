73e1543a798faf8773a24a2cc3073f234b03b4b7
Phuong Tan Thanh <phuongtanthanh99@gmail.com>
2024-07-14 11:44:17 +0700

    hehe

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # poll-app-django
+
+New postgresql container
+$ docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
+First, after configuring the database in settings.py, run the following commands:
+```bash
+python manage.py migrate
+```
+
+
+After add a new model, run the following commands to create a new migration:
+```bash
+python manage.py makemigrations polls 
+```
+(polls is the name of the app)
+If the migration is correct, run the following command to apply the migration:
+```bash
+python manage.py migrate
+```
+
+
+Run server:
+$ python manage.py runserver
--- poll_app_site/settings.py
+++ poll_app_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,17 @@ WSGI_APPLICATION = 'poll_app_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5433',  
     }
 }
 
@@ -105,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
